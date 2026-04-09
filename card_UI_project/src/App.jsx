9bd8f4c4a2e3389842001d57f1a132119c9c45f5
tausif900 <ansaritausif9268@gmail.com>
2026-04-09 19:38:53 +0530

    9/4/26

--- card_UI_project/src/App.jsx
+++ card_UI_project/src/App.jsx
@@ -1,33 +1,11 @@
 import React from "react";
 import "./App.css";
-import { Bookmark } from "lucide-react";
+import Cards from "./Components/Cards";
 
 const App = () => {
   return (
-    <div className="card">
-      <div className="top">
-        <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" />
-        <button>
-          Save <Bookmark size={15}/>
-        </button>
-      </div>
-      <div className="center">
-        <h3>
-          Amazon<span>5 days ago</span>
-        </h3>
-        <h2>Senior UI/UX Designer</h2>
-        <div className="tag">
-          <h4>Part Time</h4>
-          <h4>Senior Level</h4>
-        </div>
-      </div>
-      <div className="bottom">
-        <div>
-          <h3>$120/hr</h3>
-          <p>Mumbai, India</p>
-        </div>
-        <button>Apply Now</button>
-      </div>
+    <div className="parent">
+      <Cards/>
     </div>
   );
 };
